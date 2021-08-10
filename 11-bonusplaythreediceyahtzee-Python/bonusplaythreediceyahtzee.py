ec3ae9307b90f55839dfb762dc3a5f33b9397537
sai <saichandra123981@gmail.com>
2021-08-10 16:34:26 +0530

    5th

--- 11-bonusplaythreediceyahtzee-Python/bonusplaythreediceyahtzee.py
+++ 11-bonusplaythreediceyahtzee-Python/bonusplaythreediceyahtzee.py
@@ -38,7 +38,68 @@
 # assert(bonusPlayThreeDiceYahtzee(2333413) == (333, 29))
 # assert(bonusPlayThreeDiceYahtzee(2333555) == (555, 35))
 
+def dig(n):
+	li=[]
+	for i in str(n):
+		li.append(int(i))
+	return li
+
+def playstep2(hand, dice):
+	hand1=dig(hand)
+	dice1=dig(dice)
+	# print(len(set(hand1)))
+	if len(hand1)==len((set(hand1))):
+		new=[]
+		new.append(max(hand1))
+		new.append(dice1[-1])
+		new.append(dice1[-2])
+		# print(new)
+		new.sort()
+		new=new[::-1]
+		return ((new[0]*100) + (new[1]*10) + new[2] , (dice//100))
+	elif len(hand1)-len(set(hand1))==2:
+		return (hand,dice)
+	else:
+		new=[]
+		print(hand1)
+		if hand1[0]==hand1[1]:
+			new.append(hand1[0])
+			new.append(hand1[1])
+			new.append(dice1[-1])
+		elif hand1[1]==hand1[2]:
+			new.append(hand1[1])
+			new.append(hand1[2])
+			new.append(dice1[-1])
+		else:
+			new.append(hand1[1])
+			new.append(hand1[2])
+			new.append(dice1[-1])
+		new.sort()
+		new=new[::-1]
+		return ((new[0]*100) + (new[1]*10) + new[2] , (dice//10))
+
 
 def bonusplaythreediceyahtzee(dice):
-	# Your code goes here
-	pass
+	hand=dice%1000
+	dice//=1000
+	d=dice
+	# print(d)
+	hand,d= playstep2(hand,d)
+	print(hand,d)
+	hand,d= playstep2(hand,d)
+	hand1=dig(hand)
+	if len(hand1)==len(set(hand1)):
+		return (hand,max(hand1))
+	elif len(hand1)-len(set(hand1))==1:
+		if hand1[0]==hand1[1]:
+			return (hand,10+hand1[0]+hand1[0])
+		elif hand1[1]==hand1[2]:
+			return (hand,10+hand1[1]+hand1[1])
+		else:
+			return (hand,10+hand1[2]+hand1[2])
+	else:
+		return (hand,20+hand1[0]*3)
+
+	# return 
+
+print(bonusplaythreediceyahtzee(2333555))
