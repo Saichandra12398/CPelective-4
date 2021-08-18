b34d595f9cc818764b023014b270371d31be6eca
sai <saichandra123981@gmail.com>
2021-08-18 13:02:06 +0530

    done

--- 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
+++ 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
@@ -14,5 +14,38 @@
 # Each row and each column add to 3, but one diagonal adds to 2 and the other to 4.
 
 def ismostlymagicsquare(a):
+	colsum=sum(a[0])
+	rowsum=0
+	flag=0
+	print(colsum)
+	for i in a:
+		# print(sum(i))
+		if sum(i)!=colsum:
+			return False
+	for i in range(len(a)):
+		print(rowsum)
+		Sum=0
+		for j in range(len(a)):
+			Sum+=a[j][i]
+		if flag:
+			if rowsum!=Sum:
+				return False
+		else:
+			flag=1
+			rowsum=Sum
+	ld=0
+	rd=0
+	for i in range(len(a)):
+		for j in range(len(a)):
+			if(i==j):
+				ld+=a[i][j]
+			if(i+j==len(a)-1):
+				rd+=a[i][j]
+	print(ld,rd)
+	if ld!=rd:
+		return False
+	return True
 	# Your code goes here
 	pass
+
+print(ismostlymagicsquare([[2, 7, 6], [9, 5, 1], [4, 3, 8]]))
