13b1b3e38701fc8a8cd44a3ed12b505a476ae9ed
sai <saichandra123981@gmail.com>
2021-08-24 13:55:44 +0530

    done

--- 01-nth_happy_number-Python/nth_happy_number.py
+++ 01-nth_happy_number-Python/nth_happy_number.py
@@ -14,6 +14,34 @@
 # assert(nth_happy_number(7) == 28)
 # assert(nth_happy_number(8) == 31)
 
+def ishappynumber(n):
+	# your code goes here
+
+	x = DigiSum(n)
+	while(x>9):
+		x=DigiSum(x)
+	sol=0
+	while(x>0):
+		R = x%10
+		x =x//10
+		sol += R
+	return (sol == 1 )
+
+def DigiSum(n):
+	sum=0
+	while(n>0):
+		r = n%10
+		n = n//10
+		sum +=(r*r)
+	return sum
 
 def nth_happy_number(n):
-	return 0
+	count = 0
+	i = 0
+	while(True):
+		i+=1
+		if(ishappynumber(i)):
+			count+=1
+		if(count == n):
+			return i
+
