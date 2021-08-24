8a6a17bc79fa14c26103640119a80b6261436e39
sai <saichandra123981@gmail.com>
2021-08-24 13:54:22 +0530

    done

--- 11-ishappynumber-Python/ishappynumber.py
+++ 11-ishappynumber-Python/ishappynumber.py
@@ -1,3 +1,4 @@
+
 # ishappynumber(n) [10 pts]
 # A happy number is a number defined by the following process: Starting with any positive integer, replace the 
 # number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will 
@@ -16,7 +17,25 @@
 # assert(ishappynumber(98) == False)
 # assert(ishappynumber(404) == True)
 # assert(ishappynumber(405) == False)
-
+def DigiSum(n):
+	sum=0
+	while(n>0):
+		r = n%10
+		n = n//10
+		sum +=(r*r)
+	return sum
+	
 def ishappynumber(n):
 	# your code goes here
-	pass
+
+	x = DigiSum(n)
+	while(x>9):
+		x=DigiSum(x)
+	sol=0
+	while(x>0):
+		R = x%10
+		x =x//10
+		sol += R
+	return (sol == 1 )
+
+
