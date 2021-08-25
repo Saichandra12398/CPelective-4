1075c7ebb120f12347b08fa06f2a3afa356335e1
sai <saichandra123981@gmail.com>
2021-08-25 16:58:26 +0530

    done

--- 05-binarytreepractice-Python/binarytree.py
+++ 05-binarytreepractice-Python/binarytree.py
@@ -13,12 +13,14 @@ class BinaryTree(object):
         Return True if the find_val is in the tree and False otherwise.
         """
         # Your code goes here
-        pass
+        return self.preorder_search(self.root,find_val)
 
     def print_tree(self):
         """
         Print out all tree nodes as they are visited in a pre-order traversal."""
         # Your code goes here
+        self.preorder_print(self.root)
+
         pass
 
     def preorder_search(self, start, find_val):
@@ -26,6 +28,12 @@ class BinaryTree(object):
         Helper method - use this to create a recursive search solution.
         """
         # Your code goes here
+        if(start==None):
+            return False
+        if(start.value==find_val):
+            return True
+        return self.preorder_search(start.left,find_val) or self.preorder_search(start.right, find_val)
+
         pass
 
     def preorder_print(self, start, traversal):
@@ -33,4 +41,10 @@ class BinaryTree(object):
         Helper method - use this to create a recursive print solution.
         """
         # Your code goes here
+        if(start is None):
+            return
+        print(start.value)
+        self.preorder_print(start.left)
+        self.preorder_print(self.right)
+
         pass
