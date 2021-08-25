0d7dba692f90e977b643f810cc53949d70ecb46c
sai <saichandra123981@gmail.com>
2021-08-25 17:13:38 +0530

    done

--- 02-nearestkaprekarnumber-Python/nearestkaprekarnumber.py
+++ 02-nearestkaprekarnumber-Python/nearestkaprekarnumber.py
@@ -9,8 +9,52 @@
 # Hint: one way to solve this is to start at n and grow in each direction until you find a Kaprekar number.
 
 
+import math
 
+def kaprek(n):
+    if n==1:
+        return True
+    x=n**2
+    # print(x)
+    if len(str(x))>1:
+        left=int(str(x)[:len(str(x))//2])
+        right=int(str(x)[len(str(x))//2:])
+        # print(left,right)
+        if left+right==n:
+            return True
+        else:
+            return False
+    else:
+        return False
+    
+
+# kaprek(45)
+# def fun_nth_kaprekarnumber(n):
+#     count=0
+#     i=0
+#     while True:
+#         i+=1
+#         if kaprek(i):
+#             print(i)
+#             count+=1
+#         if count==n+1:
+#             return i
+
+# print(fun_nth_kaprekarnumber(20))
+# print(4950**2)
 import math
 
 def fun_nearestkaprekarnumber(n):
-    return 1
+    left=0
+    right=0
+    for i in range(n,0,-1):
+        if kaprek(i):
+            left=i
+            break
+    for i in range(n,n+10000,1):
+        if kaprek(i):
+            right=i
+            break
+    print(left,right)
+    x=left if n-left<=right-n else right
+    return x
